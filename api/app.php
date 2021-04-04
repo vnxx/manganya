6d03d5f5f9a1c6821579b88f6c18fbfb4aca1a3f
Kevin Adam <rexprimeadam@gmail.com>
2021-04-04 10:36:03 +0700

    Add js and css versioning #1

--- api/app.php
+++ api/app.php
@@ -3,3 +3,11 @@
 namespace api;
 
 include_once __DIR__ . '/routes.php';
+
+$html = file_get_contents('../public/index.html');
+
+//  add asset versioning
+$html = str_replace('bundle.js', 'bundle.js?version=' . filemtime('../public/build/bundle.js'), $html);
+$html = str_replace('bundle.css', 'bundle.css?version=' . filemtime('../public/build/bundle.css'), $html);
+
+echo $html;
