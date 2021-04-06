bad432a8ac5f4cd5149b18279e5267b2b41bbdc9
Kevin Adam <rexprimeadam@gmail.com>
2021-04-06 13:41:21 +0700

    Add favorite feature #4

---
+++ src/pages/favorite.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Layout from "../components/Layout.svelte";
+    import MangaItem from "../components/MangaItem.svelte";
+    import { onMount } from "svelte";
+
+    let dataset = [];
+    onMount(() => {
+        let favorites = JSON.parse(localStorage.getItem("favorites"));
+        favorites = favorites ? favorites : [];
+        dataset = favorites;
+    });
+</script>
+
+<Layout>
+    <h1 class="font-bold text-2xl">Favorites</h1>
+    <div class="grid grid-cols-2 xl:grid-cols-5 gap-6">
+        {#each dataset as data}
+            <MangaItem {data} />
+        {/each}
+    </div>
+</Layout>
