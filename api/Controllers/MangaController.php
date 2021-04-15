db86d0bb95a80505f434b006852d80dbdd39c169
Kevin Adam <rexprimeadam@gmail.com>
2021-04-15 13:55:07 +0700

    change scraping target to bacakomik

--- api/Controllers/MangaController.php
+++ api/Controllers/MangaController.php
@@ -8,18 +8,18 @@ class MangaController
 {
     public function index()
     {
-        $source = Http::get('https://komikav.com/manga/?order=update');
+        $source = Http::get('https://bacakomik.co/komik-terbaru');
 
-        preg_match_all('/<span class="type.*src="(.*)" cl/', $source, $covers);
-        preg_match_all('/<div class="tt">.*\n(.*)</', $source, $titles);
-        preg_match_all('/bsx">\n.*\n.*href=".*manga\/(.*)\//', $source, $slugs);
+        preg_match_all('/<div class="animposx">\n.*[^.*]*img src="(.*)" alt/', $source, $covers);
+        preg_match_all('/<div class="tt"> <h4>(.*)<\/h4>/', $source, $titles);
+        preg_match_all('/<div class="animposx">\n.*href=".*komik\/(.*)\/" item/', $source, $slugs);
 
         $data = [];
 
         foreach ($titles[1] as $key => $title) {
             array_push($data, [
                 'title' => $title,
-                'cover' => $covers[1][$key],
+                'cover' => $covers[1][$key] . '.jpg',
                 'slug' => $slugs[1][$key],
             ]);
         }
@@ -58,11 +58,11 @@ class MangaController
 
     public function show($slug)
     {
-        $source = Http::get('https://komikav.com/manga/' . $slug . '/');
-        preg_match_all('/data-num="(.*)">/', $source, $chapters);
+        $source = Http::get('https://bacakomik.co/komik/' . $slug . '/');
+        preg_match_all('/lchx.*<a href=".*chapter-(.*)-bah/', $source, $chapters);
         preg_match('/entry-title".*">(.*)</', $source, $title);
-        preg_match('/<div class="thumb" itemprop="image" .*\n.*src="(.*?(?="))/', $source, $cover);
-        preg_match('/description"><p>(.*[^<]*)<\/p>/', $source, $sinopsis);
+        preg_match('/<div class="thumb" itemprop="image" .*\n.*src="(.*)" tit/', $source, $cover);
+        preg_match('/description">\n.*<p>(.*[^<]*)<\/p>/', $source, $sinopsis);
 
         return [
             "title" => $title[1],
@@ -74,30 +74,25 @@ class MangaController
 
     public function showChapter($slug, $chapter)
     {
-        $source = Http::get('https://komikav.com/' . $slug . '-chapter-' . $chapter . '-bahasa-indonesia/');
-        $source2 = Http::get('https://komikav.com/manga/' . $slug . '/');
+        $source = Http::get('https://bacakomik.co/chapter/' . $slug . '-chapter-' . $chapter . '-bahasa-indonesia/');
+        $source2 = Http::get('https://bacakomik.co/komik/' . $slug . '/');
 
-        preg_match('/<div class="thumb" itemprop="image" .*\n.*src="(.*?(?="))/', $source2, $cover);
         preg_match('/entry-title".*">(.*)</', $source2, $title);
-        preg_match('/prevUrl":".*?(?<=chapter-)(.*?(?=-bah))/', $source, $prev);
-        preg_match('/nextUrl":".*?(?<=chapter-)(.*?(?=-bah))/', $source, $next);
-        preg_match('/{"source":"Server 2","images":.(.*)]}]/', $source, $mainImages);
-        preg_match_all('/"(http.*?(?="))/', $mainImages[1], $mainImages);
+        preg_match('/<div class="thumb" itemprop="image" .*\n.*src="(.*)" tit/', $source, $cover);
+        preg_match('/<a href=".*chapter-(.*)-ba.*rel="prev/', $source, $prev);
+        preg_match('/<a href=".*chapter-(.*)-ba.*rel="next/', $source, $next);
+        preg_match_all('/<img src="(.*?(?="))" alt.*?(?=Chapter)/', $source, $mainImages);
+        preg_match_all("/this.src='(.*?(?='))/", $source, $backupIamges);
 
-        // preg_match_all("/this.src='(.*?(?='))/", $source, $backupIamges);
-
-        preg_match_all('/data-num="(.*)">/', $source2, $chapters);
-
-        $mainImages[1] = array_map(function ($data) {
-            return str_replace('\/', '/', $data);
-        },  (array)$mainImages[1]);
+        $source2 = Http::get('https://bacakomik.co/komik/' . $slug . '/');
+        preg_match_all('/lchx.*<a href=".*chapter-(.*)-bah/', $source2, $chapters);
 
         $images = [];
 
-        foreach ($mainImages[1] as $image) {
+        foreach ($mainImages[1] as $key => $image) {
             array_push($images, [
                 'main_url' => $image,
-                'backup_url' => ''
+                'backup_url' => $backupIamges[1][$key]
             ]);
         }
 
