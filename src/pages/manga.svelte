863d4b05c2c3fc85e0c7153d1b1ffe10badb0321
Kevin Adam <rexprimeadam@gmail.com>
2021-05-26 09:24:02 +0700

    fix manga detail

--- src/pages/manga.svelte
+++ src/pages/manga.svelte
@@ -28,35 +28,34 @@
             .then((data) => {
                 if (data.status == "SUCCESS") {
                     dataset = data;
+                    //  check next_chapter
+                    let histories = getHistories();
+                    let h_i_open = histories
+                        .map((e) => e.slug)
+                        .indexOf(params.slug);
+                    if (
+                        h_i_open > -1 &&
+                        histories[h_i_open].history.next_chapter === null
+                    ) {
+                        let index = data.chapters.indexOf(
+                            histories[h_i_open].history.current_chapter
+                        );
+                        console.log(data.chapters.length);
+                        console.log(index);
+                        if (data.chapters.length - 1 > index) {
+                            histories[h_i_open].history.next_chapter =
+                                data.chapters[index - 1];
+                            localStorage.setItem(
+                                "histories",
+                                JSON.stringify(histories)
+                            );
+                        }
+                    }
+
+                    continueReading = histories[h_i_open];
                 } else {
                     error = data;
                 }
-
-                //  check next_chapter
-                let histories = getHistories();
-                let h_i_open = histories
-                    .map((e) => e.slug)
-                    .indexOf(params.slug);
-                if (
-                    h_i_open > -1 &&
-                    histories[h_i_open].history.next_chapter === null
-                ) {
-                    let index = data.chapters.indexOf(
-                        histories[h_i_open].history.current_chapter
-                    );
-                    console.log(data.chapters.length);
-                    console.log(index);
-                    if (data.chapters.length - 1 > index) {
-                        histories[h_i_open].history.next_chapter =
-                            data.chapters[index - 1];
-                        localStorage.setItem(
-                            "histories",
-                            JSON.stringify(histories)
-                        );
-                    }
-                }
-
-                continueReading = histories[h_i_open];
 
                 isLoading = false;
             });
