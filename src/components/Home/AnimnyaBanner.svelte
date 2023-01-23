b13341bba59bd3b1da7b18501d98e5ebe1b70b22
Kevin Adam <adamkevin520@gmail.com>
2023-01-23 19:16:29 +0700

    add animnya banner

---
+++ src/components/Home/AnimnyaBanner.svelte
@@ -0,0 +1,16 @@
+<div class="pb-5">
+	<div class="mx-auto max-w-7xl lg:flex lg:items-center lg:justify-between">
+		<h2 class="text-4xl font-bold tracking-tight text-white">
+			Tonton Anime<br /> terbaru tanpa iklan di Animnya.
+		</h2>
+		<div class="mt-10 flex items-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
+			<a
+				href="http://animnya.bykevin.work/"
+				target="_blank"
+				rel="noreferrer"
+				class="rounded-md px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 text-white bg-main-red hover:bg-main-red-hover"
+				>Buka sekarang</a
+			>
+		</div>
+	</div>
+</div>
--- src/pages/home.svelte
+++ src/pages/home.svelte
@@ -1,98 +1,99 @@
 <script>
-    // @ts-nocheck
+	// @ts-nocheck
 
-    import { onMount } from "svelte";
-    import { Link } from "svelte-navigator";
+	import { onMount } from "svelte";
+	import { Link } from "svelte-navigator";
 
-    import Layout from "../components/Layout.svelte";
-    import MangaItem from "../components/MangaItem.svelte";
-    import TypeItem from "../components/Home/TypeItem.svelte";
-    import ContributionBanner from "../components/Home/ContributionBanner.svelte";
+	import Layout from "../components/Layout.svelte";
+	import MangaItem from "../components/MangaItem.svelte";
+	import TypeItem from "../components/Home/TypeItem.svelte";
+	import ContributionBanner from "../components/Home/ContributionBanner.svelte";
 
-    import { useEffect } from "../components/hook";
+	import { useEffect } from "../components/hook";
+	import AnimnyaBanner from "../components/Home/AnimnyaBanner.svelte";
 
-    const api_url = import.meta.env.VITE_API_URL;
-    const params = new URLSearchParams(window.location.search);
+	const api_url = import.meta.env.VITE_API_URL;
+	const params = new URLSearchParams(window.location.search);
 
-    let dataset = [];
-    let continueReading = [];
-    let errorMessage = null;
-    let isLoading = true;
-    let itemWidth = 0;
-    let type = params.get("type") ?? "all";
+	let dataset = [];
+	let continueReading = [];
+	let errorMessage = null;
+	let isLoading = true;
+	let itemWidth = 0;
+	let type = params.get("type") ?? "all";
 
-    window.scrollTo(0, 0);
+	window.scrollTo(0, 0);
 
-    async function getComicks() {
-        isLoading = true;
-        const r = await fetch(`${api_url}/?type=${type}`);
-        const data = await r.json();
-        if (data.status == "SUCCESS") {
-            dataset = data.data;
-        } else {
-            dataset = [];
-            errorMessage = data.message;
-        }
-        isLoading = false;
-    }
+	async function getComicks() {
+		isLoading = true;
+		const r = await fetch(`${api_url}/?type=${type}`);
+		const data = await r.json();
+		if (data.status == "SUCCESS") {
+			dataset = data.data;
+		} else {
+			dataset = [];
+			errorMessage = data.message;
+		}
+		isLoading = false;
+	}
 
-    function updateItemWidth() {
-        let gapCount = 2;
-        let containerWidth =
-            document.getElementsByClassName("main-caontainer")[0].clientWidth;
-        if (window.innerWidth > 1024) {
-            gapCount = 5;
-        }
+	function updateItemWidth() {
+		let gapCount = 2;
+		let containerWidth =
+			document.getElementsByClassName("main-caontainer")[0].clientWidth;
+		if (window.innerWidth > 1024) {
+			gapCount = 5;
+		}
 
-        itemWidth = (containerWidth - (gapCount - 1) * 24) / gapCount;
-    }
+		itemWidth = (containerWidth - (gapCount - 1) * 24) / gapCount;
+	}
 
-    onMount(() => {
-        let histories = JSON.parse(localStorage.getItem("histories"));
-        histories = histories ? histories : [];
+	onMount(() => {
+		let histories = JSON.parse(localStorage.getItem("histories"));
+		histories = histories ? histories : [];
 
-        continueReading = histories;
+		continueReading = histories;
 
-        updateItemWidth();
+		updateItemWidth();
 
-        window.addEventListener("resize", () => {
-            updateItemWidth();
-        });
-    });
+		window.addEventListener("resize", () => {
+			updateItemWidth();
+		});
+	});
 
-    useEffect(
-        () => {
-            getComicks();
-        },
-        () => [type]
-    );
+	useEffect(
+		() => {
+			getComicks();
+		},
+		() => [type]
+	);
 
-    const typeItems = [
-        {
-            label: "Semua Komik",
-            type: "all",
-            image: null,
-        },
-        {
-            label: "Manga",
-            type: "manga",
-            image: "/assets/manga.jpeg",
-        },
-        {
-            label: "Manhua",
-            type: "manhua",
-            image: "/assets/manhua.jpeg",
-        },
-        {
-            label: "Manhwa",
-            type: "manhwa",
-            image: "/assets/manhwa.jpeg",
-        },
-    ];
+	const typeItems = [
+		{
+			label: "Semua Komik",
+			type: "all",
+			image: null,
+		},
+		{
+			label: "Manga",
+			type: "manga",
+			image: "/assets/manga.jpeg",
+		},
+		{
+			label: "Manhua",
+			type: "manhua",
+			image: "/assets/manhua.jpeg",
+		},
+		{
+			label: "Manhwa",
+			type: "manhwa",
+			image: "/assets/manhwa.jpeg",
+		},
+	];
 </script>
 
 <Layout title="Home">
-    <!-- {#if !window.matchMedia("(display-mode: standalone)").matches}
+	<!-- {#if !window.matchMedia("(display-mode: standalone)").matches}
         <section class="xl:hidden">
             <h2 class="font-bold text-2xl mb-3">Aplikasi Manganya</h2>
             <div class="flex space-x-8">
@@ -114,57 +115,57 @@
         </section>
     {/if} -->
 
-    <ContributionBanner />
+	<ContributionBanner />
 
-    {#if continueReading.length > 0}
-        <section>
-            <h2 class="font-bold text-2xl mb-5">Lanjut Baca</h2>
-            <div class="flex overflow-x-auto space-x-6">
-                {#each continueReading.slice(0, 5) as data}
-                    <MangaItem width={`${itemWidth}px`} {data} />
-                {/each}
-            </div>
-        </section>
-    {/if}
+	<AnimnyaBanner />
 
-    <section>
-        <h2 class="font-bold text-2xl mb-5">Terbaru</h2>
+	{#if continueReading.length > 0}
+		<section>
+			<h2 class="font-bold text-2xl mb-5">Lanjut Baca</h2>
+			<div class="flex overflow-x-auto space-x-6">
+				{#each continueReading.slice(0, 5) as data}
+					<MangaItem width={`${itemWidth}px`} {data} />
+				{/each}
+			</div>
+		</section>
+	{/if}
 
-        <div
-            class="flex overflow-auto mb-8 whitespace-nowrap text-sm space-x-6"
-        >
-            {#each typeItems as item}
-                <TypeItem
-                    currentType={type}
-                    {...item}
-                    onClick={() => (type = item.type)}
-                />
-            {/each}
-        </div>
+	<section>
+		<h2 class="font-bold text-2xl mb-5">Terbaru</h2>
 
-        {#if errorMessage}
-            <div class="text-center bg-red-900 text-white py-6">
-                {errorMessage}
-            </div>
-            <div class="sk-t" />
-        {/if}
+		<div class="flex overflow-auto mb-8 whitespace-nowrap text-sm space-x-6">
+			{#each typeItems as item}
+				<TypeItem
+					currentType={type}
+					{...item}
+					onClick={() => (type = item.type)}
+				/>
+			{/each}
+		</div>
 
-        <div class="grid grid-cols-2 xl:grid-cols-5 gap-6 gap-y-8">
-            {#if !isLoading}
-                {#each dataset as data}
-                    <MangaItem {data} />
-                {/each}
-            {:else}
-                {#each Array(40) as _}
-                    <MangaItem isSkeletonOnly />
-                {/each}
-            {/if}
-        </div>
-    </section>
+		{#if errorMessage}
+			<div class="text-center bg-red-900 text-white py-6">
+				{errorMessage}
+			</div>
+			<div class="sk-t" />
+		{/if}
+
+		<div class="grid grid-cols-2 xl:grid-cols-5 gap-6 gap-y-8">
+			{#if !isLoading}
+				{#each dataset as data}
+					<MangaItem {data} />
+				{/each}
+			{:else}
+				{#each Array(40) as _}
+					<MangaItem isSkeletonOnly />
+				{/each}
+			{/if}
+		</div>
+	</section>
 </Layout>
 
 <style>
-    .sk-t {
-        height: 28px;
-    }
+	.sk-t {
+		height: 28px;
+	}
 </style>
